5afd7a1d96003de0e0dacf0a5fa965ed03231bd4
Kletus Risaldi Subagio <stefanfredik@gmail.com>
2023-04-15 19:05:09 +0800

    login

--- app/Views/login/index.php
+++ app/Views/login/index.php
@@ -12,33 +12,37 @@
                             <!-- Social login form-->
                             <div class="card my-5">
                                 <div class="card-body p-5 text-center">
-                                    <div class="h3 fw-light mb-3">Login</div>
+                                    <div class="h3 fw-light mb-3">Halaman Login</div>
                                     <div>Silahkan login menggunakan akun yang sudah terdaftar.</div>
+                                    <div class="text-start">
+                                        <?= view('Myth\Auth\Views\_message_block') ?>
+                                    </div>
                                 </div>
                                 <hr class="my-0" />
                                 <div class="card-body p-5">
                                     <!-- Login form-->
-                                    <form>
+                                    <form action="<?= url_to('login') ?>" method="POST" role="form">
                                         <!-- Form Group (email address)-->
+                                        <?= csrf_field() ?>
                                         <div class="mb-3">
                                             <label class="text-gray-600 small" for="emailExample">Username</label>
-                                            <input class="form-control form-control-solid" type="text" placeholder="" aria-label="Email Address" aria-describedby="emailExample" />
+                                            <input value="<?= old('login'); ?>" class="form-control form-control-solid <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" id="username" name="login" type="text" placeholder="Masukan Username" />
+                                            <div class="invalid-feedback">
+                                                <?= session('errors.login') ?>
+                                            </div>
                                         </div>
 
                                         <div class="mb-3">
                                             <label class="text-gray-600 small" for="passwordExample">Password</label>
-                                            <input class="form-control form-control-solid" type="password" placeholder="" aria-label="Password" aria-describedby="passwordExample" />
+                                            <input class="form-control form-control-solid <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" id="password" name="password" placeholder="Masukan Password" />
+                                            <div class="invalid-feedback">
+                                                <?= session('errors.password') ?>
+                                            </div>
                                         </div>
 
 
-                                        <div class="mb-3"><a class="small" href="auth-password-social.html">Lupa Password?</a></div>
-
                                         <div class="d-flex align-items-center justify-content-between mb-0">
-                                            <div class="form-check">
-                                                <input class="form-check-input" id="checkRememberPassword" type="checkbox" value="" />
-                                                <label class="form-check-label" for="checkRememberPassword">Ingat Saya</label>
-                                            </div>
-                                            <a class="btn btn-primary" href="dashboard-1.html">Masuk</a>
+                                            <input class="btn btn-primary" type="submit" value="Login">
                                         </div>
                                     </form>
                                 </div>
